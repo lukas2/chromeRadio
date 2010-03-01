9816c0dbc4baa06be1f1e22c7388309f5cd1a061
jack10 <jack@ubilix.com>
2010-03-01 19:00:37 +0100

    Added chromeRadio.mm FreeMind File

---
+++ chromeRadio.mm
@@ -0,0 +1,12 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1267453678653" ID="ID_1594981486" MODIFIED="1267466383047" TEXT="google chrome Extension">
+<node CREATED="1267465421767" ID="ID_1184841954" MODIFIED="1267465936735" POSITION="left" TEXT="Chrome Radio">
+<node CREATED="1267465937604" ID="ID_365051291" MODIFIED="1267466014325" TEXT="Popup Mp3 Links von Einer Internetseite Anzeigen">
+<node CREATED="1267466018014" ID="ID_866810963" MODIFIED="1267466058132" TEXT="Lins werden ins Popup geholt"/>
+<node CREATED="1267466060900" ID="ID_1807077335" MODIFIED="1267466221426" TEXT="Mp3 werden Geparst"/>
+<node CREATED="1267466223397" ID="ID_683640187" MODIFIED="1267466321182" TEXT="Mp3 Links wernden angezeigt"/>
+</node>
+</node>
+</node>
+</map>
